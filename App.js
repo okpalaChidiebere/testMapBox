1403c03a846406fb1812732812b1af917ca3edc7
okpalaChidiebere <okpalacollins4@gmail.com>
2021-10-13 08:38:08 -0600

    Installing Mapbox for iOS and Android
    - Run yarn add @react-native-mapbox-gl/maps
    - Go to ios/Podfile and add the line of code. Make sure to run `cd ios && pod install && cd ..`
    - Go to android/app/build.gradle and add some line of code link in this commit
    - Then run your apps

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapboxGL from "@react-native-mapbox-gl/maps";
+
+MapboxGL.setAccessToken("<YOUR_MAPBOX_ACCESS_TOKEN>");
 
 export default function App() {
+  const [coordinates, setCoordinates] = React.useState([-73.99155, 40.73581]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.mapContainer}>
+        <MapboxGL.MapView style={styles.map}>
+          <MapboxGL.Camera zoomLevel={8} centerCoordinate={coordinates} />
+          <MapboxGL.PointAnnotation coordinate={coordinates} id="Test" />
+        </MapboxGL.MapView>
+      </View>
     </View>
   );
 }
@@ -14,8 +23,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  mapContainer: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: "blue",
+  },
+  map: {
+    flex: 1,
   },
 });
